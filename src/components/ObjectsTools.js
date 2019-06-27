3d658369398529b4b37a6eb15beacc4a6eb92423
noname <nonamemovich@gmail.com>
2019-06-28 01:38:16 +0500

    add dlist

--- src/components/ObjectsTools.js
+++ src/components/ObjectsTools.js
@@ -14,15 +14,15 @@ const ObjectsTools=function(props) {
         <div className="row">
             <div className="col-6 d-flex mr-auto font-weight-bold" style={{alignItems: "center"}}>
                 <span style={{fontSize: "22px"}}>Список Объектов</span>
-                <img src="/assets/add.svg" className="size20 ml-2 mt-1" onClick={ e => { props.showCreateForm(ShowCONST.CreateFrom) } }/>
+                <img src="assets/add.svg" className="size20 ml-2 mt-1" onClick={ e => { props.showCreateForm(ShowCONST.CreateFrom) } }/>
                 
             </div>
             <div className="col-2 pl-2"> 
                 <span className={"inlineDisplay p-2"+selectList} onClick={ e => { props.changeShowComponent(ShowCONST.List) } }> 
-                    <img src="/assets/list.svg" className="rounded float-left size30"/>
+                    <img src="assets/list.svg" className="rounded float-left size30"/>
                 </span>
                 <span className={"inlineDisplay p-2"+selectCards} onClick={ e => { props.changeShowComponent(ShowCONST.Cards) } }>
-                    <img src="/assets/table-grid.svg" className="rounded float-left size30"/>
+                    <img src="assets/table-grid.svg" className="rounded float-left size30"/>
                 </span>
             </div>
         </div>
